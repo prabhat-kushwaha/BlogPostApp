94194fdc52e6f96d2c0e22d25a810069fb78b9d7
prabhat-kushwaha <prabhatkushwaha7575@gmail.com>
2022-11-02 22:01:54 +0530

    First commit

---
+++ app/src/main/java/com/prabhatkushwaha/koinproject/remote/dto/PostRequest.kt
@@ -0,0 +1,10 @@
+package com.prabhatkushwaha.koinproject.remote.dto
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class PostRequest(
+    val body: String,
+    val title: String,
+    val userId: Int
+)
